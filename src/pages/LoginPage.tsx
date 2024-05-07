dc0730950ff3e1566039d825fe411a5c5caa621a
Andy Leon <andyleon000@gmail.com>
2024-05-07 16:25:48 -0400

    style change

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,8 @@ import GoogleSignIn from "../components/auth/GoogleSignIn";
 export default function LoginPage() {
   
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-screen">
+      <p className="text-[32px] block">Welcome to Taskify!</p>
       <GoogleSignIn />
     </div>
   )
